d4357fcbd31abceb56124eb4db9206e8958ffbac
Benjamin Barbier <benjamin.barbier.59@gmail.com>
2021-04-22 12:44:09 +0200

    :white_check_mark: : add first unit test on reducers

---
+++ apps/unicorn-ng/src/app/store/reducers/unicorns.reducers.spec.ts
@@ -0,0 +1,39 @@
+import { Unicorn } from '../../shared/models/unicorn.model';
+import * as UnicornsActions from '../actions/unicorns.actions';
+import { unicornsReducer } from './unicorns.reducers';
+
+const treeUnicorns = [
+    { id: 1 } as Unicorn,
+    { id: 2 } as Unicorn,
+    { id: 3 } as Unicorn
+];
+
+describe('unicorns.reducers', () => {
+
+    it('deleteUnicorn on deleteUnicornSuccess', () => {
+        // Given
+        const unicornToDelete: Unicorn = { id: 1 } as Unicorn;
+        const action = UnicornsActions.deleteUnicornSuccess({ unicorn: unicornToDelete });
+        const state: Unicorn[] = treeUnicorns;
+
+        // When
+        const newState = unicornsReducer(state, action);
+
+        // Then
+        expect(newState.length).toBe(2);
+    });
+
+    it('deleteUnicorn on deleteUnicorn', () => {
+        // Given
+        const unicornToDelete: Unicorn = { id: 1 } as Unicorn;
+        const action = UnicornsActions.deleteUnicorn({ unicorn: unicornToDelete });
+        const state: Unicorn[] = treeUnicorns;
+
+        // When
+        const newState = unicornsReducer(state, action);
+
+        // Then
+        expect(newState.length).toBe(3);
+    });
+
+});
