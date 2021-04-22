ce8f8857f3ea6a2beb8df44f933916a40f5b4bf5
Benjamin Barbier <benjamin.barbier.59@gmail.com>
2021-04-22 21:43:51 +0200

    :sparkles: : add localstorage store cache + shared ui component

--- apps/unicorn-ng/src/app/store/reducers/index.ts
+++ apps/unicorn-ng/src/app/store/reducers/index.ts
@@ -1,7 +1,9 @@
-import { ActionReducerMap } from '@ngrx/store';
+import { ActionReducer, ActionReducerMap, MetaReducer } from '@ngrx/store';
 import { Unicorn } from '../../shared/models/unicorn.model';
 import { cartReducer } from './cart.reducers';
 import { unicornsReducer } from './unicorns.reducers';
+import { Action } from '@ngrx/store';
+import { localStorageSync } from 'ngrx-store-localstorage';
 
 export interface EntityState {
     unicorns: Unicorn[];
@@ -13,3 +15,9 @@ export const reducers: ActionReducerMap<EntityState> = {
     cart: cartReducer,
     // here is where i put other reducers, when i have them
 };
+
+const localStorageSyncReducer = (reducer: ActionReducer<EntityState>): ActionReducer<EntityState> => {
+    return localStorageSync({ keys: ['unicorns', 'cart'], rehydrate: true })(reducer);
+};
+
+export const metaReducers: MetaReducer<EntityState, Action>[] = [localStorageSyncReducer];
